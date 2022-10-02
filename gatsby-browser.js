b556d3496397dc8a51d59d49baecc8b3cbfb5c9b
hayeon9826 <hayeonkim226@gmail.com>
2022-10-03 02:18:52 +0900

    edit css import 경로

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,11 @@
-// custom CSS styles
-require('./src/style.css')
-// normalize CSS across browsers
-require ('./src/normalize.css')
-
 // Highlighting for code blocks
 require('prism-themes/themes/prism-holi-theme.css')
 // custom typefaces
 require('typeface-montserrat')
 require('typeface-merriweather')
 
+// custom CSS styles
+// require('./src/style.css')
 
+// normalize CSS across browsers
+require ('./src/normalize.css')
