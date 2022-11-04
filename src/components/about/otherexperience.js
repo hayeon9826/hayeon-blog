57328f6c28e68304a1ec5c75d11aee11671937df
hayeon9826 <hayeonkim226@gmail.com>
2022-11-04 20:07:32 +0900

    fix: edit about

--- src/components/about/otherexperience.js
+++ src/components/about/otherexperience.js
@@ -13,13 +13,21 @@ const OtherExperience = () => {
           <h4>Open Source</h4>
           <ul className="bulleted-list">
             <li>
-              React 18 공식 도큐의 <b>"Hooks API Reference"</b> 페이지를 일부 번역하였습니다.{' '}
+              React 18 공식 도큐의{' '}
+              <a href="https://ko.reactjs.org/docs/hooks-intro.html" target="_blank">
+                Hooks API Reference
+              </a>{' '}
+              페이지를 일부 번역하였습니다.{' '}
               <a href="https://github.com/reactjs/ko.reactjs.org/pull/518" target="_blank">
                 [PR 링크]
               </a>
             </li>
             <li>
-              React 18 공식 도큐의 <b>"React 최상위 API"</b> 페이지를 일부 번역하였습니다.{' '}
+              React 18 공식 도큐의{' '}
+              <a href="https://ko.reactjs.org/docs/react-api.html" target="_blank">
+                React 최상위 API
+              </a>{' '}
+              페이지를 일부 번역하였습니다.{' '}
               <a href="https://github.com/reactjs/ko.reactjs.org/pull/517" target="_blank">
                 [PR 링크]
               </a>
