8cd9f4f86c6bb8297f87cbe18e586d767f5b5738
hayeon9826 <hayeon226@likelion.org>
2022-11-14 13:19:09 +0900

    fix: edit resume content

--- src/components/about/englishResume.js
+++ src/components/about/englishResume.js
@@ -45,8 +45,8 @@ const EnglishResume = () => {
             <h2 className="underline">Resume Objective</h2>
 
             <div className="pb-15px">
-              Front-End Web Developer with 2+ years of experience in developing web & webApp applications, optimizing
-              user interfaces, meticulous testing and debugging. Value acquiring new technologies and organizing
+              Front-End Web Developer with 2+ years of experience in developing web & webapp applications, optimizing
+              user interfaces, and meticulous testing & debugging. Value acquiring new technologies and organizing
               concepts through tech studies and tech blogs. Enjoy active code reviews, technical discussions with other
               developers, and eager to improve user experience by finding better solutions.
             </div>
@@ -64,8 +64,8 @@ const EnglishResume = () => {
                 <li>Developed and maintained web pages & webviews in Finda (Fintech Startup in Korea). </li>
                 <li>
                   Took charge of developing 'in-app agreement feature', where customers could conclude a loan contract
-                  via Finda App. Also, built desgin system and deployed to Storybook (chromatic) by cooperating with the
-                  designers.
+                  via Finda App. Also, built desgin system and deployed to Storybook (with chromatic) by cooperating
+                  with the designers.
                 </li>
                 <li>
                   Improved inefficient API call structure by implementing Redux. Decreased redundant API calls by 60%.
@@ -75,11 +75,11 @@ const EnglishResume = () => {
                   Observer.
                 </li>
                 <li>
-                  Contributed on building design system and organized common componentes on Storybook. Reduced
+                  Contributed on building design system and organized Finda Components on Storybook. Reduced
                   communication requests on Jira (Confluence Cooperation Tool) by 20%.
                 </li>
                 <li>
-                  Updated legacy code base (class function) to new functional components. Implemented Next.js SSG(Static
+                  Updated legacy code (class components) to new functional components. Implemented Next.js SSG(Static
                   Side Generation) on data fetching to improve API request time. Decreased API fetching time from 4s to
                   0.5s.
                 </li>
@@ -96,19 +96,20 @@ const EnglishResume = () => {
                 <li className="header">
                   Front-End Developer, <span>Insomenia - Seoul, Korea</span>
                 </li>
-                <li>Developed and maintained five initial startup web services using Rails (BE) and React (FE).</li>
-                <li>Applied Next.js, Node.js, Prisma for advanced front-end development and better user experience</li>
+                <li>Developed and maintained five startup web services using Ruby on Rails (BE) and React (FE).</li>
                 <li>
-                  Participated in developing code builder template by building 'Likes, Item lists, Post list, Login &
-                  Registration, Cart' features. Approximately saved 80 hours of each developer's inital setup working
-                  hours.
+                  Applied Next.js, Node.js, and Prisma for advanced front-end development and better user experience.
                 </li>
                 <li>
-                  Transferred MVP startup service to AWS service considering better security and scalability. Applied
-                  AWS Cognito on user registrations and deployed via AWS EC2 and S3.
+                  Participated in developer Insomenia web-builder template: 'Likes, Item lists, Post list, Login &
+                  Registration, Cart' features. Approximately saved 80 hours of each developer's inital setup time.
                 </li>
                 <li>
-                  Worked on more than 20 technical documents and shared the entire troubleshooting process with other
+                  Transferred MVP startup service to AWS for better security and scalability. Applied AWS Cognito on
+                  user registrations and deployed via AWS EC2 and S3.
+                </li>
+                <li>
+                  Worked on more than 20 technical documents and shared various troubleshooting processes with other
                   developers. Improved the company's 'learning development culture' by sharing problem solving process
                   with everyone.
                 </li>
@@ -127,7 +128,7 @@ const EnglishResume = () => {
                   developing various featrues, admin development, QA, and maintenance.
                 </li>
                 <li>
-                  Conducted Web Publishing referring to the design figma from desginer. Applied CSS with custom class
+                  Conducted web publishing collaborating with desginer (using figma), and applied CSS with custom class
                   names in BEM model.
                 </li>
                 <li>Improved user experience by meticulous testing and frequent discussions with the clients.</li>
@@ -177,8 +178,8 @@ const EnglishResume = () => {
                 </li>
                 <li>
                   {' '}
-                  Bachelor of <b>Creative Technology Management(CTM)</b>, Yonsei University Underwood International
-                  College
+                  Bachelor Degree of <b>Creative Technology Management(CTM)</b>, Yonsei University Underwood
+                  International College
                 </li>
                 <li>Learned Computer Science, Web Development and Digital Entrepreneurship</li>
                 <li>
